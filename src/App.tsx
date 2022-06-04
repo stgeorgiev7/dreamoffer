cd6003f607cd0e01a27be660d8c2320e8de3016e
Stoyan Georgiev <s.georgiev@dreamshot.bg>
2022-06-04 13:31:11 +0300

    feat: create solution for task-237

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Offer from "./components/Offer";
+import image from "./assets/game-designer.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+  <div className='App'>
+   <Offer
+    title='Game Designer'
+    description="Create concept arts of characters, weapons, buildings, and environments, based on the game design vision, under art director's supervision."
+    image={image}
+    tags={["color", "circle", "black", "art"]}
+   />
+  </div>
+ );
 }
 
 export default App;
